d8d83e81e33194e2a9459c45b6e6892ddc1e3190
FedGorunov <pinta.f14@gmail.com>
2018-08-18 19:21:09 +0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
 
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
@@ -11,7 +13,7 @@ if (!userArgs[0].startsWith('mongodb://')) {
     console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
     return
 }
-const mongoDB =userArgs[0];
+const mongoDB =process.env.MONGODB_URI ||userArgs[0];
 
 mongoose.connect(mongoDB);
 mongoose.Promise=global.Promise;
@@ -28,6 +30,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
